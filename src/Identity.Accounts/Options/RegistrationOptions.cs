40952dde18e4efabcd03680d0ba0c864dd63e900
Jeff Mattson <jmattson@sei.cmu.edu>
2020-06-27 23:46:59 -0400

    remove AllowedDomains default

--- src/Identity.Accounts/Options/RegistrationOptions.cs
+++ src/Identity.Accounts/Options/RegistrationOptions.cs
@@ -8,7 +8,7 @@ namespace Identity.Accounts.Options
     public class RegistrationOptions
     {
         public bool AllowManual { get; set; } = false;
-        public string AllowedDomains { get; set; } = "site.local | org.local";
+        public string AllowedDomains { get; set; }
         public bool StoreName { get; set; } = true;
         public bool StoreEmail { get; set; } = true;
         public bool AllowMultipleUsernames { get; set; } = false;
