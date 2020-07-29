b01d4001d7baacebee17336a044a240d46b51084
Jeff Mattson <jmattson@sei.cmu.edu>
2020-07-29 08:17:09 -0400

    update deps

--- src/IdentityServer/pullcdn.ps1
+++ src/IdentityServer/pullcdn.ps1
@@ -1,7 +1,7 @@
 # pullcdn.ps1
 $dst = ".\wwwroot\lib"
 mkdir -p $dst
-Invoke-WebRequest -Uri https://code.jquery.com/jquery-3.4.1.slim.min.js -OutFile $dst/jquery.slim.min.js
+Invoke-WebRequest -Uri https://code.jquery.com/jquery-3.5.1.slim.min.js -OutFile $dst/jquery.slim.min.js
 Invoke-WebRequest -Uri 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js' -OutFile $dst/popper.min.js
-Invoke-WebRequest -Uri https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js -OutFile $dst/bootstrap.min.js
-Invoke-WebRequest -Uri https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css -OutFile $dst/bootstrap.min.css
+Invoke-WebRequest -Uri https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js -OutFile $dst/bootstrap.min.js
+Invoke-WebRequest -Uri https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css -OutFile $dst/bootstrap.min.css
--- src/IdentityServer/pullcdn.sh
+++ src/IdentityServer/pullcdn.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 dst=wwwroot/lib
 mkdir -p $dst
-curl https://code.jquery.com/jquery-3.4.1.slim.min.js -o $dst/jquery.slim.min.js
+curl https://code.jquery.com/jquery-3.5.1.slim.min.js -o $dst/jquery.slim.min.js
 curl https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js -o $dst/popper.min.js
-curl https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js -o $dst/bootstrap.min.js
-curl https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css -o $dst/bootstrap.min.css
+curl https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js -o $dst/bootstrap.min.js
+curl https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css -o $dst/bootstrap.min.css
