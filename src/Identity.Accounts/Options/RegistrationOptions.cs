53a0992ffc6e5d6c91a8d5e433c290f35f2f598d
Jeff Mattson <jmattson@sei.cmu.edu>
2021-03-17 14:36:49 -0600

    make username uniqueness tag optional

--- src/Identity.Accounts/Options/RegistrationOptions.cs
+++ src/Identity.Accounts/Options/RegistrationOptions.cs
@@ -11,7 +11,15 @@ namespace Identity.Accounts.Options
         public string AllowedDomains { get; set; } = "";
         public bool StoreName { get; set; } = true;
         public bool StoreEmail { get; set; } = true;
+
+        // if true, allows users to add additional emails to their accounts
         public bool AllowMultipleUsernames { get; set; } = false;
+
+        // if true, ignores AllowedDomains check for *additional* emails
         public bool AllowAnyDomainUsernames { get; set; } = false;
+
+        // appends unique hex tag to username
+        public bool AutoUniqueUsernames { get; set; } = true;
+
     }
 }
